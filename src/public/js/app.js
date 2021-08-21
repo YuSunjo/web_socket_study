eddd6ad041ddc15ae91c25efc6fe0e11d4c7d080
yusunjo <tnswh2023@gmail.com>
2021-08-21 17:42:58 +0900

    socket xhdtls

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,40 @@
-hello;
+const messageList = document.querySelector("ul")
+const messageForm = document.querySelector("#message")
+const nickForm = document.querySelector("#nick");
 
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server");
+})
+
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+})
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from server X");
+})
+
+// setTimeout(() => {
+//     socket.send("hello from the browser!");
+// }, 10000);
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value)
+    input.value = "";
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input")
+    socket.send(input.value);
+}
+
+messageForm.addEventListener("submit", handleSubmit)
+nickForm.addEventListener("submit", handleNickSubmit)
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -6,9 +8,25 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + "/views");
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get("/*", (req, res) => res.redirect("/"));
-console.log('hello');
+// app.get("/*", (req, res) => res.redirect("/"));
 
 app.get('/', (req, res) => res.render("home"));
-const handleListen = () => console.log(`listening on 300port`);
-app.listen(3000);
+const handleListen = () => console.log(`listening on 3000 port`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server})
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconnected from Server"));
+    socket.on("message", (message) => {
+        sockets.forEach((aSocket) => aSocket.send(message));
+    })
+    socket.send("hello!!!");
+});
+
+
+server.listen(3000, handleListen);
