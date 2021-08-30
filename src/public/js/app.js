99c3c24a5a55dfaa96b0547fec9341af62f6a61c
YuSunjo <tnswh2023@gmail.com>
2021-08-30 12:19:50 +0900

    카메라 변경

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -14,26 +14,43 @@ async function getCameras() {
         const devices = await navigator.mediaDevices.enumerateDevices();
         console.log(devices);
         const cameras = devices.filter(device => device.kind === "videoinput");
-        console.log(cameras);
+        const currentCamera = myStream.getVideoTracks()[0];
         cameras.forEach(camera => {
             const option = document.createElement("option");
             option.value = camera.deviceId;
             option.innerText = camera.label;
+            if (currentCamera.label === camera.label) {
+                option.selected = true;
+            }
             camerasSelect.appendChild(option);
         })
-    }catch (e) {
+    } catch (e) {
         console.log(e);
     }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+    const initialConstrains = {
+        audio: true,
+        video: {facingMode: "user"}
+    };
+    const cameraConstrains = {
+        audio: true,
+        video: {
+            deviceId: {
+                exact: deviceId
+            }
+        }
+    }
     try {
-        myStream = await navigator.mediaDevices.getUserMedia({
-            audio: false,
-            video: true,
-        });
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId? cameraConstrains : initialConstrains
+        );
         console.log(myStream);
         myFace.srcObject = myStream;
+        if(!deviceId) {
+            await getCameras();
+        }
         await getCameras();
     } catch (e) {
         console.log(e);
@@ -70,5 +87,10 @@ function handleCameraClick() {
     }
 }
 
+async function handleCameraChange() {
+    await getMedia(camerasSelect.value)
+}
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
